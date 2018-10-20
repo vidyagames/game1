949f681cfd9ca77c3172b35b363c6beb634d2c7f
DESKTOP-RE9SN54\Micah West <micahdavidwest@gmail.com>
2018-10-20 14:00:29 -0600

    Dog Controller and colliders

---
+++ WhosAGoodBoy/Who's A Good Boy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    private float xSpeed;
+    private float ySpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var renderer = GetComponent<Renderer>();
+
+        xSpeed = Input.GetAxis("Horizontal");
+        ySpeed = Input.GetAxis("Vertical");
+        animator.SetFloat("xSpeed", xSpeed);
+        animator.SetFloat("ySpeed", ySpeed);
+
+        transform.Translate(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, 0f);
+    }
+}
