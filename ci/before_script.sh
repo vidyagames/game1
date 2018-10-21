13a048a00a53b2c6e1c1e84d96d65f82aa5c6296
nihakue <walden.gabrielw@gmail.com>
2018-10-21 03:06:06 +0100

    base64 decode license env var

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -5,5 +5,5 @@ set -x
 mkdir -p /root/.cache/unity3d
 mkdir -p /root/.local/share/unity3d/Unity/
 set +x
-echo 'Writing $UNITY_LICENSE_CONTENT to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf'
-echo "$UNITY_LICENSE_CONTENT" | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
+echo 'Decoding and writing $UNITY_LICENSE_CONTENT_ENCODED to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf'
+echo "$UNITY_LICENSE_CONTENT_ENCODED" | base64 --decode | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
