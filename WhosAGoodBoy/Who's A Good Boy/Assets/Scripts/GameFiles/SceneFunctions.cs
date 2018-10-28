d15cdc89998b0bc9433a8fbec921fe8a6c897dea
DESKTOP-RE9SN54\Micah West <micahdavidwest@gmail.com>
2018-10-28 17:32:30 -0600

    Added basic saving and loading

---
+++ WhosAGoodBoy/Who's A Good Boy/Assets/Scripts/GameFiles/SceneFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFunctions {
+
+    public static string OverWorldSceneName = "SampleScene";
+
+    public static void LoadSceneThenLoadSaveState(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        SavedGameManager.Instance.Load();
+    }
+}
