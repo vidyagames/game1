9923ed32ed703fd72765bd648b582112afa4af32
Garrett <garrettcallen@gmail.com>
2018-11-04 16:58:33 -0700

    almost there ? idk remove grt

---
+++ WhosAGoodBoy/Who's A Good Boy/Assets/Prefabs/Minigames/Barkscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barkscript : MinigameRunner {
+
+    public SpriteRenderer faceRenderer;
+    public AudioSource barkSound;
+
+    private Sprite idleface;
+    private Sprite barkingface;
+
+
+	// Use this for initialization
+	void Start () {
+        this.faceRenderer = GetComponent<SpriteRenderer>();
+        this.barkSound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey){
+            barkSound.Play();
+            print("bark");
+        }
+	}
+}
